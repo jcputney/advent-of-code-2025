d7b833a699ec35a96ef65c5ba48a7bddad2f79b8
Jonathan Putney <jputney@noverant.com>
2025-12-04 13:30:11 -0500

    Day 4 - Printing Department

---
+++ 4_1.py
@@ -0,0 +1,23 @@
+DIRS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+if __name__ == '__main__':
+    with open('input/4_1.txt', 'r') as file:
+        grid = []
+        for line in file:
+            row = []
+            for c in line.strip():
+                row.append(c)
+            grid.append(row)
+
+        total = 0
+        for i, row in enumerate(grid):
+            for j, pos in enumerate(row):
+                if pos == '@':
+                    count = 0
+                    for d in DIRS:
+                        ni, nj = i + d[0], j + d[1]
+                        if 0 <= ni < len(grid) and 0 <= nj < len(row):
+                            if grid[ni][nj] == '@':
+                                count += 1
+                    if count < 4:
+                        total += 1
+        print(total)
